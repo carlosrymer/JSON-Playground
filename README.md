d14f1ffc5d273f606b922c6b60525c6e01736570
carlosrymer <carlos.rymer@gmail.com>
2013-10-06 20:31:17 -0400

    Initializing project with first commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+JSON Playground
+===============
+
+The JSON Playground is a free online tool that allows the generation of custom json files in any structure for testing purposes. You can access the live tool [here](http://jsonplayground.com), but the source is also available to fork for other applications of the tool. It runs on AngularJS (v1.2.0-rc.2) and Foundation (v4.3.2).
+
+Requirements
+------------
+
+* Grunt
+* Ruby, RVM, and Compass
+* Gem Installation of Zurb Foundation (a Google search will help)
+* Apache/PHP
+* NodeJs (w/ NPM)
+
+Installation
+------------
+
+*	Clone the repository into a project folder of your choice and run npm install on it.
+*   Run bower install on the folder as well. You should only have AngularJS and Modernizr in your bower list (run bower list).
+*	That's pretty much all you need to do. Run grunt server to test the app or grunt build to access it from your own local server in the dist folder.
+*   Note that if you want to overwrite Foundation styles, you have to find the location of the gem installation and make your changes there.
+
---
+++ app/scripts/app.js
@@ -0,0 +1,55 @@
+/**
+ * Angular Controller to manage JSON Playground
+ * @lastupdated 10.05.2013 crymer
+ */
+
+// Define the angular module
+var jsonPlayground = angular.module('jsonPlayground', []);
+
+// Define the angular controller
+jsonPlayground.controller('jsonPlay', function($scope, $http) {
+
+	// Define some required variable
+	$scope.levels              = Modernizr.localstorage && localStorage['jsonplayLevels'] ? angular.fromJson(localStorage['jsonplayLevels']) : [{"key":"","value":"","sublevels":[]}];
+	$scope.fresh               = [{"key":"","value":"","sublevels":[]}];
+	$scope.callbackActionState = 0;
+	$scope.callback            = null;
+	$scope.callbackActionText  = ['Add', 'Remove'];
+
+	/**
+	 * Toggles the callback action state
+	 */
+	$scope.toggleCallbackState = function()
+	{
+		$scope.callbackActionState = $scope.callbackActionState ? 0 : 1;
+	}
+
+	/**
+	 * Updates the local storage based on new values
+	 */
+	$scope.updateLocalStorage = function()
+	{
+		localStorage['jsonplayLevels'] = JSON.stringify($scope.levels);
+	}
+
+	/**
+	 * Creates a new object and pushes it into an array for new levels
+	 * @param array a The array to push the new object into
+	 * @param boolean s Whether to exclude sublevels or not
+	 */
+	$scope.addLevel = function(a, s)
+	{
+		var object = !s ? {"key":"", "value": "", "sublevels": []} : {"key":"", "value": ""};
+		a.push(object);
+	}
+
+	/**
+	 * Generates the JSON file to be downloaded
+	 */
+	$scope.getJson = function()
+	{
+		var d = {"callback": ($scope.callback ? "true" : "false"), "json": JSON.stringify($scope.levels)};
+		$http({method: 'POST', url: 'http://127.0.0.1/jsonplayground/app/process.php', params: d, headers: {'Content-Type': 'application/x-www-form-urlencoded'}});
+	}
+
+});
